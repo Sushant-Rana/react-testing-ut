62a9336f15e066c3ae06bdfdc3e13c03e74e9d6e
Sushant Rana <sushant.rana@capillarytech.com>
2023-05-10 17:24:02 +0530

    testcases for chapter 2

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,22 @@ import userEvent from '@testing-library/user-event'
 import App from './App';
 
 describe('Tests for Login Form',()=>{
-    it('submitting the form calls onSubmit with username and password', () => {
+    it('submitting the form calls onSubmit with username and password', async() => {
+      let submittedData;
+      const handleSubmit = data => (submittedData = data);
+      render (<App onSubmit={handleSubmit} />)
+      let user=screen.getByLabelText(/username/i);
+      let password=screen.getByLabelText(/password/i);
+      await userEvent.type(user,'UserName');
+      await userEvent.type(password,'Password');
+      const submit=screen.getByRole('button', {  name: /submit/i});
+      userEvent.click(submit);
+      expect(submittedData.username).toEqual('UserName');
+      expect(submittedData.password).toContain('Password')
+      
+
+      
+
         // create a variable called "submittedData" and a handleSubmit function that
         // accepts the data and assigns submittedData to the data that was submitted
         // Hint: if you need a hand, here's what the handleSubmit function should do:
