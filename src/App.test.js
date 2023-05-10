4d58cb45b9c8cf60829a12cc1ab1f934143fcd45
Sushant Rana <sushant.rana@capillarytech.com>
2023-05-10 16:41:41 +0530

    tetcases for chapter1

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,23 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
 describe('Tests for Counter App',()=>{
-    it('Should have initial value 0', () => {
+    it('Test cases', () => {
       render(<App />);
+      const initialCalue=screen.getByText(/0/i);
+      expect(initialCalue).toHaveTextContent(0);
+      const increment=screen.getByRole('button', {
+        name: /increment/i
+      });
+      userEvent.click(increment);
+      expect(initialCalue).toHaveTextContent(1);
+      const decrement=screen.getByRole('button', {
+        name: /decrement/i
+      });
+      userEvent.click(decrement);
+      expect(initialCalue).toHaveTextContent(0);
+
       /** 
        * Todo:
        * 1. Assert that the initial value is 0. Use getBy/findBy Query to find text '0' from the screen.
